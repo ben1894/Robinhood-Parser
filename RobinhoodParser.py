f46dbe7e955d307ff801c0ba942b44be6baf461e
Ben Drees <50091002+ben1894@users.noreply.github.com>
2021-07-20 17:25:45 -0500

    File Cropping Tests

--- RobinhoodParser.py
+++ RobinhoodParser.py
@@ -1,5 +1,5 @@
 from tika import parser  
-
+from PyPDF2 import PdfFileWriter,PdfFileReader,PdfFileMerger
 
 class Stock:
     name = ""
@@ -21,6 +21,17 @@ class Options:
 tickerNames = []
 tickerDict = {}
    
+pdf_file = PdfFileReader(open("","rb"))
+page = pdf_file.getPage(0)
+print(page.cropBox.getLowerLeft())
+print(page.cropBox.getLowerRight())
+print(page.cropBox.getUpperLeft())
+print(page.cropBox.getUpperRight())
+#page.mediaBox.lowerRight = (lower_right_new_x_coordinate, lower_right_new_y_coordinate)
+#page.mediaBox.lowerLeft = (lower_left_new_x_coordinate, lower_left_new_y_coordinate)
+#page.mediaBox.upperRight = (upper_right_new_x_coordinate, upper_right_new_y_coordinate)
+#page.mediaBox.upperLeft = (upper_left_new_x_coordinate, upper_left_new_y_coordinate)
+
 # opening pdf file
 parsed_pdf = parser.from_file("829234bf-09f3-4f86-aa70-731c04efd559.pdf")
   
