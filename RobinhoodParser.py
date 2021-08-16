f25827aa828dba555f53d9c57639f92568efbbd0
Ben Drees <50091002+ben1894@users.noreply.github.com>
2021-08-15 23:26:04 -0500

    Regular Parsing Start

--- RobinhoodParser.py
+++ RobinhoodParser.py
@@ -28,48 +28,12 @@ data = parsed_pdf['content']
 print(data)
 
 
+lines = data.splitLines()
+activityPrev = lines.find("Account Activity", 0)
+while activityPrev != -1:
+    activityPrev = lines.find("Account Activity", activityPrev)
+    currentLine = lines[activityPrev].split()
 
-
-# 1. Crop top of page and check for string "Account Summary"
-    # Store page object to list if it does
-# 2. If it exists crop and parse the rest
-# 3. If it say Executed trades pending settlement do otherwise  
-
-
-
-#last page of active or nah is weird
-
-#webbrowser.open(page)
-#page[]
-
-#page.mediaBox.lowerRight = (lower_right_new_x_coordinate, lower_right_new_y_coordinate)
-#page.mediaBox.lowerLeft = (lower_left_new_x_coordinate, lower_left_new_y_coordinate)
-#page.mediaBox.upperRight = (upper_right_new_x_coordinate, upper_right_new_y_coordinate)
-#page.mediaBox.upperLeft = (upper_left_new_x_coordinate, upper_left_new_y_coordinate)
-
-# opening pdf file
-#parsed_pdf = parser.from_file("829234bf-09f3-4f86-aa70-731c04efd559.pdf")
-  
-# saving content of pdf
-# you can also bring text only, by parsed_pdf['text'] 
-# parsed_pdf['content'] returns string 
-#data = parsed_pdf['content'] 
-  
-# Printing of content 
-#print(data)
-  
-# <class 'str'>
-#print(type(data))
-
-
-
-
-# 0, 612
-# |
-# |
-# |
-# |
-# |___________ 792, 0
-
-
-# 0,0    792,0     0,612    792,612
+pendingPrev = lines.find("Executed Trades Pending Settlement", 0)
+while pendingPrev != -1:
+    pendingPrev = lines.find("Executed Trades Pending Settlement", pendingPrev)
